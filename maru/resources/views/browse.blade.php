c5eda382f4ce21b27dca2f72465b134071e04a88
greenlapok <greenlapok@gmail.com>
2017-11-04 23:00:03 +0800

    add codemirror resouces

--- maru/resources/views/browse.blade.php
+++ maru/resources/views/browse.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel @ BROWSE
                 </div>
 
                 <div class="links">
--- maru/resources/views/request.blade.php
+++ maru/resources/views/request.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel @ REQUEST
                 </div>
 
                 <div class="links">
